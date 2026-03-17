1d313fc5e895c1b72b06be43618391174311a8f2
Adam Eivy <atomantic@gmail.com>
2026-03-17 14:42:54 -0700

    fix: remove unused exportToNotes UIKit method and use flexibleCategory.name
    Remove dead code exportToNotes that depended on UIViewController, and
    use flexibleCategory.name instead of category.rawValue for correct
    pack prompt category display in markdown export.

--- Ideator/Services/ExportManager.swift
+++ Ideator/Services/ExportManager.swift
@@ -1,54 +1,17 @@
 import Foundation
-import UIKit
 
 class ExportManager {
     static let shared = ExportManager()
-    
+
     private init() {}
-    
-    func exportToNotes(_ ideaList: IdeaList, from viewController: UIViewController) {
-        let content = ideaList.formattedForExport
-        
-        let activityViewController = UIActivityViewController(
-            activityItems: [content],
-            applicationActivities: nil
-        )
-        
-        activityViewController.excludedActivityTypes = [
-            .addToReadingList,
-            .assignToContact,
-            .openInIBooks,
-            .postToVimeo,
-            .postToWeibo,
-            .postToFlickr,
-            .postToTwitter,
-            .postToFacebook,
-            .postToTencentWeibo
-        ]
-        
-        if let popoverController = activityViewController.popoverPresentationController {
-            popoverController.sourceView = viewController.view
-            popoverController.sourceRect = CGRect(
-                x: viewController.view.bounds.midX,
-                y: viewController.view.bounds.midY,
-                width: 0,
-                height: 0
-            )
-            popoverController.permittedArrowDirections = []
-        }
-        
-        viewController.present(activityViewController, animated: true) {
-            PersistenceManager.shared.saveCompleted(ideaList)
-        }
-    }
-    
+
     func exportAsText(_ ideaList: IdeaList) -> String {
         ideaList.formattedForExport
     }
     
     func exportAsMarkdown(_ ideaList: IdeaList) -> String {
         var output = "# \(ideaList.prompt.formattedTitle)\n\n"
-        output += "**Category:** \(ideaList.prompt.category.rawValue)\n"
+        output += "**Category:** \(ideaList.prompt.flexibleCategory.name)\n"
         output += "**Created:** \(ideaList.createdDate.formatted(date: .long, time: .shortened))\n\n"
         
         output += "## Ideas\n\n"
